d1d36febc5d76617130b5ce16fcf6ddb35f40eba
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-13 00:57:06 +0900

    added sending event to event bus

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commenetsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex')
   const { content } = req.body
 
@@ -23,7 +24,21 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commenetsByPostId[req.params.id] = comments
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  })
+
   res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+  console.log('Event Received', req.body.type)
+  res.send({})
 })
 
 app.listen(4001, () => {
