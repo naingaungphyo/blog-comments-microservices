26d4231343d46d4fdce6f3183ab5a11338d6787f
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-14 00:18:46 +0900

    finish moderation service

--- index.js
+++ index.js
@@ -8,36 +8,60 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-const commenetsByPostId = {}
+const commentsByPostId = {}
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commenetsByPostId[req.params.id] || [])
+  res.send(commentsByPostId[req.params.id] || [])
 })
 
 app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex')
   const { content } = req.body
 
-  const comments = commenetsByPostId[req.params.id] || []
+  const comments = commentsByPostId[req.params.id] || []
 
-  comments.push({ id: commentId, content })
+  comments.push({ id: commentId, content, status: 'pending' })
 
-  commenetsByPostId[req.params.id] = comments
+  commentsByPostId[req.params.id] = comments
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   })
 
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event Received', req.body.type)
+
+  const { type, data } = req.body
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data
+    const comments = commentsByPostId[postId]
+
+    const comment = comments.find((comment) => {
+      return comment.id === id
+    })
+
+    comment.status = status
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
+
   res.send({})
 })
 
